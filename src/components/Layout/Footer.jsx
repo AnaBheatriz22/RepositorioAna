8719afd1dc1544e15c31f638a402c1232fef35a1
AnaBheatriz22 <bheatrisa979@gmail.com>
2026-01-24 20:03:03 -0300

    Primeiro sub do portfolio

--- src/components/Layout/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-    return (
-         <footer>
-        <h2>© 2025 Empresa Ana Bheatriz. Todos os direitos reservados.</h2>
-         </footer>  
-    );
-}
-export default Footer;
